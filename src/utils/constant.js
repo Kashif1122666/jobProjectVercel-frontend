af425cf1a6a74ba4c93e6361c7f09a309cd7ac48
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2025-03-29 20:49:16 +0500

    change end point to use railway live backend/

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="https://complete-job-portal-app-with-mern-stack-3uma.vercel.app/api/v1/user";
-export const JOB_API_END_POINT="https://complete-job-portal-app-with-mern-stack-3uma.vercel.app/api/v1/job";
-export const APPLICATION_API_END_POINT="https://complete-job-portal-app-with-mern-stack-3uma.vercel.app/api/v1/application";
-export const COMPANY_API_END_POINT="https://complete-job-portal-app-with-mern-stack-3uma.vercel.app/api/v1/company";
+export const USER_API_END_POINT="https://jobporjectvercel-backend-production.up.railway.app/api/v1/user";
+export const JOB_API_END_POINT="https://jobporjectvercel-backend-production.up.railway.app/api/v1/job";
+export const APPLICATION_API_END_POINT="https://jobporjectvercel-backend-production.up.railway.app/api/v1/application";
+export const COMPANY_API_END_POINT="https://jobporjectvercel-backend-production.up.railway.app/api/v1/company";
